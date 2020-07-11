d3d885039fdd20eb3f9bd71b1050a54133afc9c4
vinit-churi <vinitchuri@hotmail.com>
2020-07-11 15:01:04 +0530

    :beers: finished with the functional prototype

--- src/components/coin/Coin.js
+++ src/components/coin/Coin.js
@@ -5,7 +5,9 @@ export default class Coin extends Component{
     render(){
         return(
             <div className='coin'>
-
+                <div className='image_container'>
+                    <img src={this.props.img} alt='coin'/>
+                </div>
             </div>
         )
     }
--- src/components/coinfliiper/CoinFliper.js
+++ src/components/coinfliiper/CoinFliper.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import './CoinFliper.css';
 import Coin from '../coin/Coin';
 export default class CoinFliper extends Component {
+    static defaultProps={
+        coins: [
+            {side : 'heads', img : 'https://jkscoinworld.com/wp-content/uploads/2018/05/2013-a.jpg'},
+            {side : 'tales', img : 'https://jkscoinworld.com/wp-content/uploads/2018/05/2013-b.jpg'}
+        ]
+    }
+    constructor(props){
+        super(props);
+        this.state={
+            counter: 0,
+            heads: 0,
+            tales: 0,
+            currentSide: this.props.coins[0]
+        };
+        this.handleFlip=this.handleFlip.bind(this);
+    }
+    
+    handleFlip(){
+        const random = Math.floor(Math.random()*2);
+        // console.log(random);
+        this.setState({
+            currentSide : this.props.coins[random]
+        });
+        this.setState(currState => {
+            return {counter : currState.counter + 1}
+        })
+        if(random){
+            this.setState(currState => {
+                return {heads : currState.heads + 1}
+            })
+        }else{
+            this.setState(currState => {
+                return {tales : currState.tales + 1}
+            })
+        }
+
+    }
+
     render(){
         return(
             <div className='coinfliper'>
-                <Coin/>
+                <h1>let's flip a coin</h1>
+                <Coin img={this.state.currentSide.img}/>
+                <button onClick={this.handleFlip}>flip!</button>
+                <h3>{`heads: ${this.state.heads} tales: ${this.state.tales} counter: ${this.state.counter}`}</h3>
             </div>
         )
     }
